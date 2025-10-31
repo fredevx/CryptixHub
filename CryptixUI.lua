ec54f3051aba14a51555d2e8c5da6b7ed3b98ac8
Shedletsky <santinbon@gmail.com>
2025-10-31 18:46:59 -0500

    Replaced CryptixUI.lua with new full build (v1.4.1)

--- CryptixUI.lua
+++ CryptixUI.lua
@@ -18,33 +18,6 @@ EnsureFolder(base)
 EnsureFolder(hub)
 EnsureFolder(hub .. "/config")
 EnsureFolder(hub .. "/mods")
---==================================================
---  CryptixUI (Based on CryptixUI v1.6.57)
---  Author: fredevx
---  Core UI Engine for CryptixHub
---  Includes:
---     * AnimEngine
---     * Dock System
---     * Themes
---     * Discord Stats
---     * Config System
---==================================================
---[[
-     _      ___         ____  ______
-    | | /| / (_)__  ___/ / / / /  _/
-    | |/ |/ / / _ \/ _  / /_/ // /  
-    |__/|__/_/_//_/\_,_/\____/___/
-    
-    v1.6.57  |  2025-10-27  |  Roblox UI Library for scripts
-    
-    This script is NOT intended to be modified.
-    To view the source code, see the `src/` folder on the official GitHub repository.
-    
-    Author: Footagesus (Footages, .ftgs, oftgs)
-    Github: https://github.com/Footagesus/CryptixUI
-    Discord: https://discord.gg/ftgs-development-hub-1300692552005189632
-    License: MIT
-]]
 
 local a a={cache={}, load=function(b)if not a.cache[b]then a.cache[b]={c=a[b]()}end return a.cache[b].c end}do function a.a()return{
 Dialog="Accent",
